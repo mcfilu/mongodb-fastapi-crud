0fffb98082dee284dee9fb3f38c7da856808acf2
mcfilu <filip.1olszewski@gmail.com>
2024-03-07 10:20:22 +0000

    clearing the crud/users file

--- app/crud/users.py
+++ app/crud/users.py
@@ -1,5 +1,7 @@
 import pprint
 from database.init import users_collections
+from schemas.users import User
+from bson.objectid import ObjectId
 
 printer = pprint.PrettyPrinter()
 
@@ -13,19 +15,13 @@ def insert_user():
     id = users_collections.insert_one(new_user).inserted_id
     print(id)
 
-# insert_user()
 
 def get_all():
     all_users = users_collections.find_one({"experience_years": 4})
     print(all_users)
-    # list_users = list(all_users)
-    # print(list_users)
-    # print(list_users[0])
-    # print('asdfas')
     for user in all_users:
         printer.pprint(user)
 
-# get_all()
 
 def get_one(user_id):
     _id = ObjectId(user_id)
@@ -33,7 +29,7 @@ def get_one(user_id):
     printer.pprint(user)
 
 
-# get_one("65e90d331f0c26e56a602b13")
+
 
 
 
@@ -43,7 +39,7 @@ def get_some_cols():
     for user in users:
         printer.pprint(user)
 
-# get_some_cols()
+
         
 def query_users_by_exp(start, end):
     query = {
@@ -57,16 +53,11 @@ def query_users_by_exp(start, end):
     for user in result:
         printer.pprint(user)
 
-query_users_by_exp(2,8)
 
 def delete_by_id(user_id):
     _id = ObjectId(user_id)
     deleted = users_collections.delete_one({"_id": _id})
-    print(deleted.deleted_count)
-
-# delete_by_id("65e90b4cadc232407580175a")
-    
-
+    print(deleted.deleted_count)    
 
 
 
@@ -75,6 +66,4 @@ def update_user_by_id(user_id, data):
     updated = users_collections.update_one({"_id": _id}, {"$set": {"first_name": data.first_name, "last_name": data.last_name, "occupancy": data.occupancy, "experience_years": data.experience_years}})
     print(updated.modified_count)
 
-new_user = User(first_name="Lazio", last_name="Olsu", occupancy="Softweare", experience_years=5)
 
-# update_user_by_id("65e90d331f0c26e56a602b13", new_user)
